d973c491a41be68da3d29f410705b470c80e20ee
Robert Gwynn-Jones <gwynnr@gmail.com>
2017-07-05 00:13:34 +1000

    Complete Zend 3 tutorial steps

--- config/modules.config.php
+++ config/modules.config.php
@@ -11,9 +11,14 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\InputFilter',
+    'Zend\Filter',
+    'Zend\Hydrator',
+    'Zend\I18n',
     'Zend\Form',
     'Zend\Db',
     'Zend\Router',
     'Zend\Validator',
     'Application',
+    'Album'
 ];
