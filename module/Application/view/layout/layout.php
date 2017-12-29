cab4f9136da186c2e642e51215fd3ff48b22b373
Rob Gwynn-Jones <gwynnr@gmail.com>
2017-12-29 16:58:54 +1100

    Mark appropriate header section as active even when in sub-route, rather than just in main route

--- module/Application/view/layout/layout.php
+++ module/Application/view/layout/layout.php
@@ -1,11 +1,17 @@
 <?php
 
+function route_url_startswith($uri, $request_uri)
+{
+    $len = strlen($uri);
+    return substr($request_uri, 0, $len) === $uri;
+}
+
 function route_url_matches($uri, $default=False)
 {
     $request_uri = $_SERVER['REQUEST_URI'];
     return
         ($request_uri === '/' && $default) ||
-        $request_uri === $uri;
+        route_url_startswith($uri, $request_uri);
 }
 
 function get_header_active_class($uri, $default=False)
